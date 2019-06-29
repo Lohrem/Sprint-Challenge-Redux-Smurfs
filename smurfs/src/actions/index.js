7825be0f508273e50263193c8f6892aee7adc54a
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-06-29 13:40:05 -0400

    base functionaly for add a smurf is working

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,14 @@
-/* 
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios'
 
+export const GET_SMURF = "GET_SMURF"
+export const GET_SMURF_SUCCESS = "GET_SMURF_SUCCESS"
+export const GET_SMURF_FAIL = "GET_SMURF_FAIL"
+
+export const ADD_SMURF = "ADD_SMURF"
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +19,16 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: GET_SMURF})
+  axios
+    .get(`http://localhost:3333/smurfs`)
+    .then(res => dispatch({ type: GET_SMURF_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: GET_SMURF_FAIL, payload: err }))
+}
+export const addSmurf = smurf => {
+  return {
+    type: ADD_SMURF,
+    payload: { name: smurf.name, age: smurf.age, height: smurf.height }
+  }
+}
